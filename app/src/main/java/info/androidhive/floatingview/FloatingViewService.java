ae497d93ba41c0f4d97216aa7444b202494a5718
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-06-12 01:10:58 +0530

    Floating view service

--- app/src/main/java/info/androidhive/floatingview/FloatingViewService.java
+++ app/src/main/java/info/androidhive/floatingview/FloatingViewService.java
@@ -1,21 +1,33 @@
 package info.androidhive.floatingview;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class FloatingViewService extends Service {
 
     private WindowManager mWindowManager;
     private View mFloatingView;
+    FrameLayout frameLayout;
+    private int camId= Camera.CameraInfo.CAMERA_FACING_FRONT;
+    private Camera camera;
 
     public FloatingViewService() {
     }
@@ -48,6 +60,23 @@ public class FloatingViewService extends Service {
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mWindowManager.addView(mFloatingView, params);
 
+
+
+
+        /*
+        * windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+    //here is all the science of params
+    final LayoutParams myParams = new LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            LayoutParams.TYPE_SYSTEM_ERROR,
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                    | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            PixelFormat.TRANSLUCENT
+    );
+        * */
+
         //The root element of the collapsed view layout
         final View collapsedView = mFloatingView.findViewById(R.id.collapse_view);
 
@@ -61,6 +90,14 @@ public class FloatingViewService extends Service {
                 stopSelf();
             }
         });
+
+
+        //open camera
+        frameLayout = (FrameLayout) mFloatingView.findViewById(R.id.collapsed_iv);
+        camera = Camera.open(camId);
+
+        ShowCamera showCamera = new ShowCamera(this, camera);
+        frameLayout.addView(showCamera);
 
 
         //Open the application on thi button click
@@ -131,5 +168,72 @@ public class FloatingViewService extends Service {
     public void onDestroy() {
         super.onDestroy();
         if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+    }
+}
+
+
+
+class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
+    private Camera camera;
+    private SurfaceHolder holder;
+
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        Camera.Parameters parameters = camera.getParameters();
+
+        //change the orientation of the camera
+
+        List<Camera.Size> sizes=parameters.getSupportedPictureSizes();
+        Camera.Size mSize=null;
+
+        for (Camera.Size size: sizes)
+        {
+            mSize=size;
+        }
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+            parameters.set("orientation", "portrait");
+            camera.setDisplayOrientation(90);
+            parameters.setRotation(90);
+        } else {
+            parameters.set("orientation", "landscape");
+            camera.setDisplayOrientation(0);
+            parameters.setRotation(0);
+        }
+
+        parameters.setPictureSize(mSize.width,mSize.height);
+
+
+        camera.setParameters(parameters);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
     }
 }
